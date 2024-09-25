b47850b8c3ec02b7f291d92679b583a6b74609b6
guilafont1 <128362642+guilafont1@users.noreply.github.com>
2024-09-25 10:10:51 +0200

    vfinal

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,75 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Equipment;
+use App\Entity\Loan;
+use App\Entity\State;
+use App\Entity\Stock;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
 
+        $states = [];
+        for ($i = 0; $i < 10; $i++) {
+            $state = new State();
+            $state ->setDescription($faker->word());
+            $manager->persist($state);
+            $states[] = $state;
+        }
+
+        $stocks = [];
+        for ($i = 0; $i < 10; $i++) {
+            $stock = new Stock();
+            $stock->setType($faker->word());
+            $stock->setQuantity($faker->randomNumber());
+            $manager->persist($stock);
+            $stocks[] = $stock;
+        }
+
+        $userRegular = new User();
+        $userRegular->setEmail($faker->name());
+        $userRegular->setEmail('testRegularUser@gmail.com');
+        $userRegular->setPassword($this->hasher->hashPassword($userRegular, 'test'));
+        $manager->persist($userRegular);
+        $users[] = $userRegular;
+
+        $equipments = [];
+        for ($i = 0; $i < 10; $i++) {
+            $equipment= new Equipment();
+            $equipment->setName($faker->name());
+            $equipment->setDescription($faker->word());
+            $equipment->setAvailable($faker->boolean());
+            $equipment->setCreatedAt($faker->dateTime());
+            $equipment->setReference($faker->word());
+            $equipment->setStateId($faker->randomElement($states));
+            $equipment->setTypeId($faker->randomElement($stocks));
+            $manager->persist($equipment);
+            $equipments[] = $equipment;
+        }
+
+        $loans = [];
+        for ($i = 0; $i < 10; $i++) {
+            $loan= new Loan();
+            $loan->setUserId($faker->randomElement($users));
+            $loan->setEquipmentId($faker->randomElement($equipments));
+            $loan->setStartTime($faker->dateTime());
+            $loan->setEndTime($faker->dateTime());
+            $loan->setStatus($faker->word());
+            $loan->setCreatedAt($faker->dateTime());
+            $manager->persist($loan);
+            $loans[] = $loan;
+        }
         $manager->flush();
     }
 }
