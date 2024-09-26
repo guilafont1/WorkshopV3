6b948887d55b715bb68fc4fa870ab3b9ecd02189
Nouster <djebali.mohamed88@gmail.com>
2024-09-26 11:01:59 +0200

    link to state admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Equipment;
+use App\Entity\State;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -45,5 +46,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Équipements', 'fas fa-list', Equipment::class);
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('États', 'fas fa-list', State::class);
     }
 }
